08acceb8c91175b562081143576085e3b2957477
Yangjie404 <160984707+Yangjie404@users.noreply.github.com>
2024-07-08 09:21:53 +0800

    Update app2.py

--- app2.py
+++ app2.py
@@ -14,7 +14,7 @@ import streamlit as st
 
 
 #导入数据
-csv_data = pd.read_csv(r'D:\\Radiomics\\marvel1.csv') # 读取data1.csv
+csv_data = pd.read_csv("marvel1.csv") # 读取data1.csv
 all_data = csv_data[["sex", "age", "af","glucose","NIHSS","ASPECTS","ASITN","OCCT","TOAST"]]
 all_data["sex"] = all_data["sex"].replace(["male", "female"], [1, 0])
 all_data["af"] = all_data["af"].replace(["yes", "no"], [1, 0])
